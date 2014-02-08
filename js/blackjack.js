4800f440bfcc15e9ce6abc0d43180846468257d2
kingtak <kingtak.w@gmail.com>
2014-02-08 11:21:32 -1000

    created the deck itself
    fixed problems with strings

--- js/blackjack.js
+++ js/blackjack.js
@@ -1,9 +1,11 @@
-function Deck{
-	var hearts = createSuit(H);
-	var spades = createSuit(S);
-	var clubs = createSuit(C);
-	var diamonds = createSuit(D);
+function Deck(){
 
+	this.hearts = createSuit("H");
+	this.spades = createSuit("S");
+	this.clubs = createSuit("C");
+	this.diamonds = createSuit("D");
+	this.deck = createDeck(hearts,spades,clubs,diamonds);
+	
 	function createSuit(suitName){
 		var cardSuit = [];
 		for(var i=2; i<=10; i++){
@@ -14,5 +16,16 @@ function Deck{
 		cardSuit.push("K_"+suitName);
 		cardSuit.push("A_"+suitName);
 		return cardSuit;
+	};
+
+	function createDeck(Hearts, Spades, Clubs, Diamonds){
+		var deck = [];
+		for(var i =0; i<4; i++ ){
+			var suit = arugments[i];
+			for(var k =0; k<13; k++){
+				deck.push(suit[k]);
+			}
+		}
+		return deck;
 	}
 }
