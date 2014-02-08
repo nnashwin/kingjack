e35fe003960cc3fb48d62d0102589ff7697751aa
Ray Farias <ray@rayfarias.com>
2014-02-08 12:45:52 -1000

    added card dealing animations

--- js/blackjack_table.js
+++ js/blackjack_table.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
 	//patience is a virture or something
+	var top_card_count = 0;
+	var btm_card_count = 0;
 
-	//Buttons
+	//Buttons & actions
 	$('#strt_btn').on("click", function(){
 		console.log("Start Game");
 		$(this).hide();
@@ -16,17 +18,47 @@ $(document).ready(function(){
 		console.log("Pressed Hold");
 	});
 
-	$('#top-player-area').append("<div class=\"guiCard\"></div>");
-	$('#top-player-area').append("<div class=\"guiCard\"></div>");
-	$('#top-player-area').append("<div class=\"guiCard\"></div>");
-	$('#top-player-area').append("<div class=\"guiCard\"></div>");
-	$('#top-player-area').append("<div class=\"guiCard\"></div>");
 
-	$('#btm-player-area').prepend("<div class=\"guiCard\"></div>");
-	$('#btm-player-area').prepend("<div class=\"guiCard\"></div>");
-	$('#btm-player-area').prepend("<div class=\"guiCard\"></div>");
-	$('#btm-player-area').prepend("<div class=\"guiCard\"></div>");
-	$('#btm-player-area').prepend("<div class=\"guiCard\"></div>");
+	//Debug Tools
+	$('#db-deal-top').on("click", function(){
+		guiDealCard("top-player");
+	});
 
+	$('#db-deal-btm').on("click", function(){
+		guiDealCard("btm-player");
+	});
+
+	// Functions
+	// 1. Hand Creation
+	function guiBuildHand(player){
+		var which_player = player;
+		for (var i = 1; i < 6; i++){
+			$('#' + which_player).append("<div class=\"guiCard\" id=\"" + which_player + "-" + i + "\"></div>");
+		}
+	}
+
+	function guiInitHands(){
+		guiBuildHand("top-player");
+		guiBuildHand("btm-player");
+		$('.guiCard').fadeTo(0, 0);
+	}
+
+	// 2. Dealing Triggers
+	function guiDealCard(player){
+
+		//get Card here
+
+		var which_player = player;
+		var which_card = ++top_card_count;
+		if (which_player === "btm-player"){
+			which_card = ++btm_card_count;
+		}
+		$('#' + which_player + "-" + which_card).fadeTo(1000, 1);
+	}
+
+	// 3. Restart game
+
+	//Initialize
+	guiInitHands();
 
 });
