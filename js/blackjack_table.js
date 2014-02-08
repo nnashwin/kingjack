c5edde1c208dc55b05e6a4c96d003939ef91cd29
Ray Farias <ray@rayfarias.com>
2014-02-08 11:29:08 -1000

    added basic playfield

--- js/blackjack_table.js
+++ js/blackjack_table.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
 	//patience is a virture or something
-	
+	$('#strt_btn').on("click", function(){
+		console.log("Start Game");
+		$(this).hide();
+		initGame();
+	});
+
+	function initGame(){
+		
+	}
 
 });
