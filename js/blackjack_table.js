fff2af0ec4d01ec1879c5824228433a2f2242acf
Ray Farias <ray@rayfarias.com>
2014-02-08 12:18:39 -1000

    completed basic playfield's view

--- js/blackjack_table.js
+++ js/blackjack_table.js
@@ -1,13 +1,32 @@
 $(document).ready(function(){
 	//patience is a virture or something
+
+	//Buttons
 	$('#strt_btn').on("click", function(){
 		console.log("Start Game");
 		$(this).hide();
 		initGame();
 	});
 
-	function initGame(){
-		
-	}
+	$('#hit').on("click", function(){
+		console.log("Pressed Hit");
+	});
+
+	$('#hold').on("click", function(){
+		console.log("Pressed Hold");
+	});
+
+	$('#top-player-area').append("<div class=\"guiCard\"></div>");
+	$('#top-player-area').append("<div class=\"guiCard\"></div>");
+	$('#top-player-area').append("<div class=\"guiCard\"></div>");
+	$('#top-player-area').append("<div class=\"guiCard\"></div>");
+	$('#top-player-area').append("<div class=\"guiCard\"></div>");
+
+	$('#btm-player-area').prepend("<div class=\"guiCard\"></div>");
+	$('#btm-player-area').prepend("<div class=\"guiCard\"></div>");
+	$('#btm-player-area').prepend("<div class=\"guiCard\"></div>");
+	$('#btm-player-area').prepend("<div class=\"guiCard\"></div>");
+	$('#btm-player-area').prepend("<div class=\"guiCard\"></div>");
+
 
 });
