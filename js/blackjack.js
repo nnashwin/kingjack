fc2b9ba9b8f46c9196969d492d017d2b418bf8b4
kingtak <kingtak.w@gmail.com>
2014-02-08 10:38:22 -1000

    Revert "Revert "createSuit function created""
    This reverts commit 8208757fc55d309adcef077266ceecfa5c554e94.

--- js/blackjack.js
+++ js/blackjack.js
@@ -4,7 +4,13 @@ function Deck{
 	var clubs;
 	var diamonds;
 
-	function createSuit(SuitName){
-		
+	function createSuit(suitName){
+		var cardSuit = [];
+		for(var i=2; i<=10; i++){
+			cardSuit.push(i+"_"+suitName);
+		}
+		cardSuit.push("J_"+suitName);
+		cardSuit.push("Q_"+suitName);
+		cardSuit.push	
 	}
 }
