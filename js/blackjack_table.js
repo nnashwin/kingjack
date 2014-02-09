17c2d1e6895f8fe4fa284b7335cf9006e3364865
Ray Farias <ray@rayfarias.com>
2014-02-08 15:11:18 -1000

    build for engine testing

--- js/blackjack_table.js
+++ js/blackjack_table.js
@@ -1,57 +1,57 @@
-$(document).ready(function(){
+$(document).ready(function() {
 	//patience is a virture or something
 	var deck_position = $('#guiDeck').position();
 	var top_card_count = 0;
 	var btm_card_count = 0;
 
 	//Buttons & actions
-	$('#strt_btn').on("click", function(){
+	$('#strt_btn').on("click", function() {
 		console.log("Start Game");
 		$(this).hide();
 		initGame();
 	});
 
-	$('#hit').on("click", function(){
+	$('#hit').on("click", function() {
 		console.log("Pressed Hit");
 	});
 
-	$('#hold').on("click", function(){
+	$('#hold').on("click", function() {
 		console.log("Pressed Hold");
 	});
 
 
 	//Debug Tools
-	$('#db-deal-top').on("click", function(){
+	$('#db-deal-top').on("click", function() {
 		guiDealCard("top-player");
 	});
 
-	$('#db-deal-btm').on("click", function(){
+	$('#db-deal-btm').on("click", function() {
 		guiDealCard("btm-player");
 	});
 
 	// Functions
 	// 1. Hand Creation
-	function guiBuildHand(player){
-		var which_player = player;
-		for (var i = 1; i < 6; i++){
-			$('#' + which_player).append("<div class=\"guiCard\" id=\"" + which_player + "-" + i + "\"></div>");
-		}
-	}
+	// function guiBuildHand(player) {
+	// 	var which_player = player;
+	// 	for (var i = 1; i < 6; i++) {
+	// 		$('#' + which_player).append("<div class=\"guiCard\" id=\"" + which_player + "-" + i + "\"></div>");
+	// 	}
+	// }
 
-	function guiInitHands(){
+	function guiInitHands() {
 		guiBuildHand("top-player");
 		guiBuildHand("btm-player");
 		$('.guiCard').fadeTo(0, 0);
 	}
 
 	// 2. Dealing Triggers
-	function guiDealCard(player){
+	function guiDealCard(player) {
 
 		//get Card here
 
 		var which_player = player;
 		var which_card = ++top_card_count;
-		if (which_player === "btm-player"){
+		if (which_player === "btm-player") {
 			which_card = ++btm_card_count;
 		}
 		$('#' + which_player + "-" + which_card).fadeTo(1000, 1);
@@ -61,5 +61,12 @@ $(document).ready(function(){
 
 	//Initialize
 	guiInitHands();
+
+	//beta brah
+	var card_pos = $('#top-player-1').position();
+	$('#guiDeck').animate({
+		top: card_pos.top,
+		left: card_pos.left
+	}, 10000);
 
 });
