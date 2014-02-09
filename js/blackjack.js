183117fccedca79c8321ed117a50fa573c6ca1ed
Nobu <tyler.boright@gmail.com>
2014-02-08 15:32:19 -1000

    added gui functionality, still have to write rules

--- js/blackjack.js
+++ js/blackjack.js
@@ -196,5 +196,60 @@ $(document).ready(function(){
 	var dealthand = d.deal(2);
 	console.log(dealthand);
 	console.log(blackJackDealer.handvalue(dealthand));
+	//patience is a virture or something
+	var deck_position = $('#guiDeck').position();
+	var top_card_count = 0;
+	var btm_card_count = 0;
+
+	//Buttons & actions
+	$('#strt_btn').on("click", function() {
+		console.log("Start Game");
+		$(this).hide();
+		initGame();
+	});
+
+	$('#hit').on("click", function() {
+		console.log("Pressed Hit");
+	});
+
+	$('#hold').on("click", function() {
+		console.log("Pressed Hold");
+	});
+
+
+	//Debug Tools
+	$('#db-deal-top').on("click", function() {
+		guiDealCard("top-player");
+	});
+
+	$('#db-deal-btm').on("click", function() {
+		guiDealCard("btm-player");
+	});
+
+	// Functions
+	// 1. Hand Creation
+	// function guiBuildHand(player) {
+	// 	var which_player = player;
+	// 	for (var i = 1; i < 6; i++) {
+	// 		$('#' + which_player).append("<div class=\"guiCard\" id=\"" + which_player + "-" + i + "\"></div>");
+	// 	}
+	// }
+
+
+
+	// 2. Dealing Triggers
+	function guiDealCard(player) {
+
+		//get Card here
+
+		var which_player = player;
+		var which_card_position = ++top_card_count;
+		if (which_player === "btm-player") {
+			which_card = ++btm_card_count;
+		}
+		$('#' + which_player + "-" + which_card_position).fadeTo(1000, 1);
+	}
+
 
 });
+
